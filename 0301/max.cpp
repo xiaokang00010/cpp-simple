88c3b89b1b16ccb67e1cff019deedb222c158333
xiaokang00010 <47490867+xiaokang00010@users.noreply.github.com>
2020-03-01 13:25:59 +0800

    Create max.cpp

---
+++ 0301/max.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n,a[110];
+int f(int n){
+  if (n == 1)  return a[1];
+  return max(f(n - 1),a[n])
+}
+
+int main(){
+  cin >> n;
+  for(int i = 1;i <= n;i++){
+    cin >>  a[i];
+  }
+  cout <<　f(n);
+}
